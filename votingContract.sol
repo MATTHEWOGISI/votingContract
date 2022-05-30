67850a08e63dcefb3e2a1282d15c605916ab07f2
OGISI MATTHEW <okemygreat4all@live.com>
2022-05-30 16:29:32 +0100

    implemented the interface functions

--- votingContract.sol
+++ votingContract.sol
@@ -6,7 +6,9 @@ pragma solidity >=0.7.0 <0.9.0;
  * @title Ballot
  * @dev Implements voting process along with vote delegation
  */
-contract VotingContract {
+import "https://github.com/Ekolance/Voting-Smart-Contract-Interface/blob/main/IVotingContract.sol";
+
+contract VotingContract is IVotingContract {
    
     struct Voter {
         uint weight; // weight is accumulated by delegation
@@ -15,36 +17,23 @@ contract VotingContract {
         uint vote;   // index of the voted proposal
     }
 
-    struct Proposal {
+    struct candidate {
         // If you can limit the length to a certain number of bytes, 
         // always use one of bytes1 to bytes32 because they are much cheaper
-        bytes32 name;   // short name (up to 32 bytes)
+        uint candidateId;
+        bytes32 candidateName;   // short name (up to 32 bytes)
         uint voteCount; // number of accumulated votes
     }
 
-    address public chairperson;
+    address public chairMan;
 
     mapping(address => Voter) public voters;
 
-    Proposal[] public proposals;
+    candidate[] public candidates;
 
-    /** 
-     * @dev Create a new ballot to choose one of 'proposalNames'.
-     * @param proposalNames names of proposals
-     */
-    constructor(bytes32[] memory proposalNames) {
-        chairperson = msg.sender;
-        voters[chairperson].weight = 1;
-
-        for (uint i = 0; i < proposalNames.length; i++) {
-            // 'Proposal({...})' creates a temporary
-            // Proposal object and 'proposals.push(...)'
-            // appends it to the end of 'proposals'.
-            proposals.push(Proposal({
-                name: proposalNames[i],
-                voteCount: 0
-            }));
-        }
+    constructor() {
+        chairMan = msg.sender;
+        voters[chairMan].weight = 1;       
     }
     
     /** 
@@ -53,8 +42,8 @@ contract VotingContract {
      */
     function giveRightToVote(address voter) public {
         require(
-            msg.sender == chairperson,
-            "Only chairperson can give right to vote."
+            msg.sender == chairMan,
+            "Only chairMan can give right to vote."
         );
         require(
             !voters[voter].voted,
@@ -85,7 +74,7 @@ contract VotingContract {
         if (delegate_.voted) {
             // If the delegate already voted,
             // directly add to the number of votes
-            proposals[delegate_.vote].voteCount += sender.weight;
+            candidates[delegate_.vote].voteCount += sender.weight;
         } else {
             // If the delegate did not vote yet,
             // add to her weight.
@@ -107,21 +96,21 @@ contract VotingContract {
         // If 'proposal' is out of the range of the array,
         // this will throw automatically and revert all
         // changes.
-        proposals[proposal].voteCount += sender.weight;
+        candidates[candidate].voteCount += sender.weight;
     }
 
     /** 
      * @dev Computes the winning proposal taking all previous votes into account.
      * @return winningProposal_ index of winning proposal in the proposals array
      */
-    function winningProposal() public view
-            returns (uint winningProposal_)
+    function winningCandidate() public view
+            returns (uint winningCandidate_)
     {
         uint winningVoteCount = 0;
-        for (uint p = 0; p < proposals.length; p++) {
-            if (proposals[p].voteCount > winningVoteCount) {
-                winningVoteCount = proposals[p].voteCount;
-                winningProposal_ = p;
+        for (uint p = 0; p < candidates.length; p++) {
+            if (candidates[p].voteCount > winningVoteCount) {
+                winningVoteCount = candidates[p].voteCount;
+                winningCandidate_ = p;
             }
         }
     }
@@ -133,6 +122,42 @@ contract VotingContract {
     function winnerName() public view
             returns (bytes32 winnerName_)
     {
-        winnerName_ = proposals[winningProposal()].name;
+        winnerName_ = candidates[winningCandidate()].name;
     }
+
+  function addCandidate(string name) external override returns(bool)
+  {       
+            // 'Proposal({...})' creates a temporary
+            // Proposal object and 'proposals.push(...)'
+            // appends it to the end of 'proposals'.
+            uint newCandidateId = candidates.length + 1 ;
+
+            candidates.push(candidate({
+                candidateId: newCandidateId,
+                candidateName: name ,
+                voteCount: 0
+            }));        
+  }
+    
+    function voteCandidate(uint candidateId) external override returns(bool)
+    {
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight != 0, "Has no right to vote");
+        require(!sender.voted, "Already voted.");
+        sender.voted = true;
+        sender.vote = candidate;
+
+        // If 'proposal' is out of the range of the array,
+        // this will throw automatically and revert all
+        // changes.
+        candidates[candidate].voteCount += sender.weight;
+    }
+
+    //getWinner returns the name of the winner
+    function getWinner() external override returns(bytes32)
+    {
+        
+    }
+
+
 }
